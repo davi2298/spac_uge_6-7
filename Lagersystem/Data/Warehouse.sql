8db96226080d4810e1aa2853fcbcb5c6b7fda136
David Ajken <46527057+davi2298@users.noreply.github.com>
2025-04-04 13:56:26 +0200

    Database setup (#14)
    * Ready for the creation of the db
    
    * Update .env Example
    
    * cleanup

---
+++ Lagersystem/Data/Warehouse.sql
@@ -0,0 +1,3 @@
+INSERT INTO warehouses (Id) VALUE ('55cc93fe-efd2-42b3-853f-46007c72c729');
+INSERT INTO warehouses (Id) VALUE ('6a6c3b4c-b02d-4479-94f4-c7a4fa91276d');
+INSERT INTO warehouses (Id) VALUE ('7b48807a-b8b9-4439-9bab-9b6a0ab9f636');
